8ed895040ef3de37258dfbca449f7085d016abae
Ram-8585 <janakiram8585@gmail.com>
2026-02-23 16:07:43 +0530

    dummy

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,9 +1,9 @@
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Mnagement System");
+        System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 01");
         System.out.println("System intiallized successfully");
 
-
+// dummy
     }
 }
